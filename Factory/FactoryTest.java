377a2424c3fa45a7545f6a2f6cf75659ba5a8086
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-20 17:02:07 +0530

    MODIFIED :Utility Class of Factory test

--- Factory/FactoryTest.java
+++ Factory/FactoryTest.java
@@ -2,10 +2,13 @@ package com.DesignPattern.Factory;
 
 public class FactoryTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		//PC CONFIG
 		Computer pc = ComputerFactory.getComputer("pc", "2 GB", "500 GB","2.5 GHz");
 		System.out.println("PC Config "+pc);
-
+		//SERVER CONFIG
+		Computer server = ComputerFactory.getComputer("server", "32 GB" ,"2 TB", "2.9 GHz");
+		System.out.println("Server Config "+server);
 	}
-
 }
