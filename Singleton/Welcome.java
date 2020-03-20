8bb611b397efa3d688c630dbdf2b6418e4a19e75
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-20 15:15:05 +0530

    ADDED:Welcome to Singleton pattern

---
+++ Singleton/Welcome.java
@@ -0,0 +1,11 @@
+package com.DesignPattern.Singleton;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to  Singleton Design Pattern");
+		// TODO Auto-generated method stub
+
+	}
+
+}
