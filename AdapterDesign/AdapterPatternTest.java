ef693d8fbc604a7fe544650564d6f1bc2f51ea62
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-22 21:23:31 +0530

    ADDED:Utility class of Class Adapter

--- AdapterDesign/AdapterPatternTest.java
+++ AdapterDesign/AdapterPatternTest.java
@@ -4,7 +4,7 @@ public class AdapterPatternTest {
 
 	public static void main(String[] args) {
 
-		//testClassAdapter();
+		testClassAdapter();
 		testObjectAdapter();
 	}
 	private static void testObjectAdapter()
@@ -17,7 +17,16 @@ public class AdapterPatternTest {
 		System.out.println("v12 volts using Object Adapter="+v12.getVolts());
 		System.out.println("v120 volts using Object Adapter="+v120.getVolts());
 	}
-	
+	private static void testClassAdapter() 
+	{
+		SocketAdapter sockAdapter = new SocketClassAdapterImpl();
+		Volt v3 = getVolt(sockAdapter, 3);
+		Volt v12 = getVolt(sockAdapter, 12);
+		Volt v120 = getVolt(sockAdapter, 120);
+		System.out.println("v3 volts using Class Adapter=" + v3.getVolts());
+		System.out.println("v12 volts using Class Adapter=" + v12.getVolts());
+		System.out.println("v120 volts using Class Adapter=" + v120.getVolts());
+	}
 	private static Volt getVolt(SocketAdapter sockAdapter, int i)
 	{
 		switch (i) 
