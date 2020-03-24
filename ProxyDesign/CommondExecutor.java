1aa1a7c48af7df5d089f742959ee89e2ba55819a
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-24 14:55:28 +0530

    ADDED :CommondExecutor

--- ProxyDesign/CommondExecutor.java
+++ ProxyDesign/CommondExecutor.java
@@ -2,8 +2,9 @@ package com.DesignPattern.ProxyDesign;
 
 import java.io.IOException;
 
-public interface CommondExecutor {
+public interface CommondExecutor 
+{
 	public void runCommand(String command) throws IOException;
 
-	}
+}
 
