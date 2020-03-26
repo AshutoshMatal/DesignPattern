43745d11997b5c41c7da43a7a8d3434159225685
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-26 14:35:21 +0530

    ADDED :Chat mediator Implementation

---
+++ Mediator/ChatMediatorImpl.java
@@ -0,0 +1,30 @@
+package com.DesignPattern.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatMediatorImpl implements ChatMediator {
+	private List<User> users;
+
+	public ChatMediatorImpl() {
+		this.users = new ArrayList<>();
+	}
+
+	@Override
+	public void sendMessage(String message, User user) {
+		for (User user1 : this.users) {
+			if (user1 != user)
+				user1.recieve(message);
+		}
+
+	}
+
+	@Override
+	public void addUser(User user) {
+		this.users.add(user);
+
+	}
+
+}
+
+
