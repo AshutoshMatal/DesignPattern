7b561c51e44a93fe22bdfc25720688c67b45114e
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-26 14:31:17 +0530

    ADDED :User Implementation to send and receive message

---
+++ Mediator/UserImpl.java
@@ -0,0 +1,24 @@
+package com.DesignPattern.Mediator;
+
+public class UserImpl extends User {
+
+	public UserImpl(ChatMediator mediator, String name) {
+		super(mediator, name);
+
+	}
+
+	@Override
+	public void send(String message) {
+		System.out.println(this.name +": Sending message " + message);
+		mediator.sendMessage(message, this);
+
+	}
+
+	@Override
+	public void recieve(String message) {
+		System.out.println(this.name +": Recieve message " + message);
+
+	}
+
+}
+
